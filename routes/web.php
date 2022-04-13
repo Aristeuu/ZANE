4e30cb0f0185d7325cfdcf7867e2e092cb7c2ab4
unknown <aristeuwalker@gmail.com>
2022-04-13 20:36:41 +0100

    login

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProdutosController;
+use App\Http\Controllers\loginController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,6 +17,14 @@ use App\Http\Controllers\ProdutosController;
 */
 
 Route::get('/', [HomeController::class, 'index'])->name('inicio');
+
+Route::get('/login',function()
+{
+    return view('auth.login');
+})->name('showLogin')->middleware('guest');
+
+Route::post('/login',[loginController::class, 'login'])->name('login');
+
 
 Route::post('/teste',[HomeController::class, 'teste'])->name('teste');
 

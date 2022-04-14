f386d55b1b353ea1ca09211070dc743e087c4f82
unknown <aristeuwalker@gmail.com>
2022-04-14 14:09:52 +0100

    singleProduto

--- resources/views/portal/produtosShow.blade.php
+++ resources/views/portal/produtosShow.blade.php
@@ -10,37 +10,47 @@
 @push('body')
     
     
-  <!-- product section -->
+<section class="about_section">
+    <div class="container-fluid  ">
+      <div class="row">
+        <div class="col-md-5 ml-auto">
+          <div class="detail-box pr-md-3">
+            <div class="heading_container">
+              <h2>
+                    {{$produto->nome}}
+              </h2>
+            </div>
+            <p>
+                {!!$produto->descricao!!}
+            </p>
+            
+            <p>
+                PREÇO A PAGAR : 
+              @if($produto->desconto != null)
+                <s></s>
+                    {{number_format( ($produto->preco - (($produto->desconto/100)*$produto->preco) + $local->preco ) ,2)}} <span>Kz</span> 
+              @else
+                    {{number_format($produto->preco + $local->preco, 2)}} <span>Kz</span> 
+              @endif 
 
-  <section class="product_section layout_padding">
-    <div class="container">
-      <div class="heading_container heading_center">
-        <h2>
-            {{$produto->nome}}
-        </h2>
+            </p>
+            
+            
+            <!--a href="">
+              Read More
+            </a-->
+
+          
+          </div>
+        </div>
+        <div class="col-md-6 px-0">
+          <div class="img-box">
+            <img src="public/images/{{$produto->foto}}" alt="">
+          </div>
+        </div>
       </div>
-      <div class="row">        
-            <div class="col-sm-6 col-lg-4">
-              <div class="box">
-                <div class="img-box">
-                <img src="/public/images/{{$produto->foto}}" alt="">                    
-                </div>                
-                <div class="detail-box">                  
-                  
-                </div>
-              </div>
-            </div>                  
-    </div>
-      <!--div class="btn_box">
-        <a href="" class="view_more-link">
-          View More
-        </a>
-      </div-->
     </div>
   </section>
 
-  <!-- end product section -->
-
-    nome
-
+  <!-- end about section -->
 @endpush

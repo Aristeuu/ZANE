292e5ba4603899c34bc923e636b2e5202621d3a3
unknown <aristeuwalker@gmail.com>
2022-04-14 14:31:47 +0100

    whatsapp

--- resources/views/portal/produtosShow.blade.php
+++ resources/views/portal/produtosShow.blade.php
@@ -25,14 +25,18 @@
             </p>
             
             <p>
-                PREÇO A PAGAR : 
-              @if($produto->desconto != null)
-                <s></s>
-                    {{number_format( ($produto->preco - (($produto->desconto/100)*$produto->preco) + $local->preco ) ,2)}} <span>Kz</span> 
-              @else
-                    {{number_format($produto->preco + $local->preco, 2)}} <span>Kz</span> 
-              @endif 
-
+                PREÇO A PAGAR :
+                <b> 
+                    @if($produto->desconto != null)
+                        <s></s>
+                            {{number_format( ($produto->preco - (($produto->desconto/100)*$produto->preco) + $local->preco ) ,2)}} <span>Kz</span> 
+                    @else
+                            {{number_format($produto->preco + $local->preco, 2)}} <span>Kz</span> 
+                    @endif
+                </b> 
+                <div class="whatsapp">
+                    <a href="https://api.whatsapp.com/send?l=pt-BR&phone=244948806223" target="_blank" class="whatsapp_link"> <i class="fa fa-whatsapp"></i></a>
+              </div>
             </p>
             
             
@@ -45,7 +49,7 @@
         </div>
         <div class="col-md-6 px-0">
           <div class="img-box">
-            <img src="/public/images/{{$produto->foto}}" alt="">
+            <img src="/public/images/{{$produto->foto}}" alt="" width="200px" >
           </div>
         </div>
       </div>
